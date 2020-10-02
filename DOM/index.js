030f663bc0e2c08f3f22d29c4a5be54687d4cf79
jaya sinha <jaya.sinha@accenture.com>
2020-10-02 12:31:30 +0530

    Basic DOM manipulation

---
+++ DOM/index.js
@@ -0,0 +1,6 @@
+document.querySelector(".list a").style.color="Red";
+document.querySelector("button").style.backgroundColor="yellow"
+document.querySelector("h1").classList.add("huge");
+alert("Pause");
+document.querySelector("h1").textContent = "GoodBye";
+document.querySelector("a").setAttribute("href","https://www.bing.com");
