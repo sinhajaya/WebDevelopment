8deaf1151d9483e548936ff20378db3908d51eff
jaya sinha <jaya.sinha@accenture.com>
2020-10-18 19:15:34 +0530

    Intro-to-node and npm

---
+++ Intro-to-Node/index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes"); //includes the superheroes package from npm
+var mySuperhero = superheroes.random();
+console.log("mySuperhero = "+mySuperhero);
+
+var supervillains = require("supervillains");
+var myVillain = supervillains.random();
+console.log("MyVillain = "+myVillain);
+
